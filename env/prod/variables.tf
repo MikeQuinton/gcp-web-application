529a9431cdd6f02fa6ce6c98e26dfe18dc8308ee
Michael Quinton <32737081+MikeQuinton@users.noreply.github.com>
2022-07-19 02:06:35 +0100

    Update variables.tf

--- env/prod/variables.tf
+++ env/prod/variables.tf
@@ -1,31 +1,31 @@
 locals {
-    gcp_service_account_name = "${var.project}-svc"
-    cloud_sql_instance_name = "${var.project}-db"
-    mig_instance_name = "app-flask-vm"
+  gcp_service_account_name = "${var.project}-svc"
+  cloud_sql_instance_name  = "${var.project}-db"
+  mig_instance_name        = "app-flask-vm"
 }
 
 variable "region" {
-    type = string
-    description = "Default region used for deployment"
-    default = "europe-west2"
+  type        = string
+  description = "Default region used for deployment"
+  default     = "europe-west2"
 }
 
 variable "zone" {
-    type = string
-    description = "Default zone used for deployment"
-    default = "europe-west2-c"
+  type        = string
+  description = "Default zone used for deployment"
+  default     = "europe-west2-c"
 }
 
 variable "project" {
-    type = string
-    description = "Name of the project"
-    default = "appsbroker-web-application"
+  type        = string
+  description = "Name of the project"
+  default     = "appsbroker-web-application"
 }
 
 variable "network_name" {
-    type = string
-    description = "Name for the VPC network"
-    default = "app-network"
+  type        = string
+  description = "Name for the VPC network"
+  default     = "app-network"
 }
 
 variable "subnet_range" {
@@ -40,27 +40,27 @@ variable "subnet_name" {
 }
 
 variable "database_version" {
-    type = string
-    description = "Database version for app"
-    default = "MYSQL_8_0"
+  type        = string
+  description = "Database version for app"
+  default     = "MYSQL_8_0"
 }
 
 variable "database_tier" {
-    type = string
-    description = "Database tier for app"
-    default = "db-f1-micro"
+  type        = string
+  description = "Database tier for app"
+  default     = "db-f1-micro"
 }
 
 variable "database_name" {
-    type = string
-    description = "Name of database for app"
-    default = "app-db"
+  type        = string
+  description = "Name of database for app"
+  default     = "app-db"
 }
 
 variable "machine_type" {
-    type = string
-    description = "Compute type to deploy"
-    default = "e2-medium"
+  type        = string
+  description = "Compute type to deploy"
+  default     = "e2-medium"
 }
 
 variable "source_image_family" {
@@ -76,25 +76,23 @@ variable "source_image_project" {
 }
 
 variable "mig_size" {
-    type = number
-    description = "Number of instances"
-    default = 1
+  type        = number
+  description = "Number of instances"
+  default     = 1
 }
 
 variable "services" {
-    type = list(string)
-    description = "List of services to enable for project"
-    default = [
-        "compute.googleapis.com",
-        "appengine.googleapis.com",
-        "appengineflex.googleapis.com",
-        "cloudbuild.googleapis.com",
-        "secretmanager.googleapis.com",
-        "servicenetworking.googleapis.com",
-        "iam.googleapis.com",
-        "storage.googleapis.com",
-        "cloudbuild.googleapis.com",
-        "cloudresourcemanager.googleapis.com",
-        "serviceusage.googleapis.com"
-    ]
+  type        = list(string)
+  description = "List of services to enable for project"
+  default = [
+    "compute.googleapis.com",
+    "appengine.googleapis.com",
+    "appengineflex.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "secretmanager.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "iam.googleapis.com",
+    "storage.googleapis.com",
+    "cloudbuild.googleapis.com"
+  ]
 }
